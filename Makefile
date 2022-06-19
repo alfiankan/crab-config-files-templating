9041cf8a157134f901e7e25af3692265fecd8d49
alfiankan <alfiankan19@gmail.com>
2022-06-20 04:52:34 +0700

    make: add makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+test:
+	go test ./replacer... -v
+e2e:
+	sh ./test.sh
--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
+#!/bin/sh
+
 go run ./cmd/... \
     -f nginx-deployment.yaml \
     -r namespace=production \
     -r exposePort=8081 \
     -q traceUrl=http://trace.domain.com \
     -q authUrl=http://auth.domain.com \
+    -v \
     -o result.yaml
